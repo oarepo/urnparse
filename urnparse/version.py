883b5c9082dae1f631840073cf8b1415103ddb19
Miroslav Bauer <mirekys@users.noreply.github.com>
2024-05-13 10:46:04 +0200

    chore(release) bump package version

--- urnparse/version.py
+++ urnparse/version.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2021 Miroslav Bauer @ CESNET.
+# Copyright (C) 2024 CESNET.
 #
 # urnparse is free software; you can redistribute it and/or modify
 # it under the terms of the MIT License; see LICENSE file for more details.
 
 """Python library for generating and parsing and RFC 8141 compliant uniform resource names (URN)."""
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
